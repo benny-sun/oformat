2ac492d4a94188e704987ab7c5b96b289fa0c62a
Hao Sun <ben831001@gmail.com>
2017-05-21 15:53:29 +0800

    Delete blade.php

--- Sortable/demo3/view/blade.php
+++
@@ -1,9 +0,0 @@
-<?php foreach ($rows as $key => $row) { ?>
-
-<div data-id="<?php echo $row['id']?>" class="list-group-item">
-    <span class="badge"><?php echo $row['id']?></span>
-    <span class="glyphicon glyphicon-move" aria-hidden="true"></span>
-    &nbsp;&nbsp;&nbsp;&nbsp;<?php echo $row['data']?>
-</div>
-
-<?php }?>
